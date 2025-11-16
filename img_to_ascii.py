569bfdd856ee0973ee14fed2e602b988e75b3ec3
Fede <fede.atzeni372@gmail.com>
2025-11-16 13:27:08 +0100

    first

---
+++ img_to_ascii.py
@@ -0,0 +1,32 @@
+from PIL import Image # py 3.14 not supported
+import os
+
+ascii_chars = "@%#*+=-:. "
+# ascii_chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+def pixel_to_ascii(pixel):
+    return ascii_chars[pixel * len(ascii_chars) // 256] # pixel è un valore 0-255 (scala di grigi)
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(BASE, "img.jpg")
+
+img = Image.open(PATH)
+img = img.resize((125, 75))  # resize
+img = img.convert("L")       # scale of gray
+
+ascii_art = []
+
+for y in range(img.height):
+    row = ""
+    for x in range(img.width):
+        pixel = img.getpixel((x, y))
+        row += pixel_to_ascii(pixel)
+    ascii_art.append(row)
+    
+base_path = os.path.dirname(os.path.abspath(__file__))
+output_path = os.path.join(base_path, "output.txt")
+
+with open(output_path, "w") as f:
+    f.write("\n".join(ascii_art))
+    
+print("Done!")
